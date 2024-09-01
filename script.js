eb83ccfeb32e69fa50f0cace5daf16a59c82f3c0
Jesus Gil <lagg312@gmail.com>
2024-09-01 20:01:27 -0300

    ajuste colores, imagenes, logos

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // Puedes agregar aquí cualquier funcionalidad JavaScript necesaria
-document.addEventListener('DOMContentLoaded', function() {
-    // Ejemplo: Efecto al hacer scroll
-    const services = document.querySelectorAll('.service');
-    const observer = new IntersectionObserver(entries => {
-        entries.forEach(entry => {
-            if (entry.isIntersecting) {
-                entry.target.classList.add('visible');
-            } else {
-                entry.target.classList.remove('visible');
-            }
-        });
-    }, { threshold: 0.1 });
+document.addEventListener("DOMContentLoaded", function () {
+  // Ejemplo: Efecto al hacer scroll
+  const services = document.querySelectorAll(".service");
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("visible");
+        } else {
+          entry.target.classList.remove("visible");
+        }
+      });
+    },
+    { threshold: 0.1 }
+  );
 
-    services.forEach(service => {
-        observer.observe(service);
-    });
+  services.forEach((service) => {
+    observer.observe(service);
+  });
 });
